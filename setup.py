607030061355e8802440b6dd856c02439cc660ff
Eric Tsai <eric@mbientlab.com>
2019-05-27 23:58:54 -0700

    Added logging messages to setup file

--- setup.py
+++ setup.py
@@ -5,26 +5,37 @@ from shutil import move
 from subprocess import call, STDOUT
 from setuptools import setup
 from setuptools.command.build_py import build_py
-
+import logging
 import os
 import platform
 import sys
+
+logging.basicConfig(level=logging.INFO)
 
 machine = "arm" if "arm" in platform.machine() else ("x64" if sys.maxsize > 2**32 else "x86")
 root = os.path.dirname(os.path.abspath(__file__))
 warble = os.path.join(root, 'clibs', 'warble')
 dest = os.path.join("mbientlab", "warble")
 
+
+def _execute(**kwargs):
+    logging.info("%s (args = %s, cwd = %s)" % (kwargs["msg"], str(kwargs["args"]), kwargs["root"]))
+
+    if call(kwargs["args"], cwd=kwargs["root"], stderr=STDOUT) != 0:
+        raise RuntimeError(kwargs["error_msg"])
+
+
 class WarbleClean(clean):
     def run(self):
-        if (platform.system() == 'Windows'):
+        if platform.system() == 'Windows':
             dll = os.path.join(dest, "warble.dll")
             if os.path.isfile(dll):
                 os.remove(dll)
-        elif (platform.system() == 'Linux'):
+        elif platform.system() == 'Linux':
             for f in os.listdir(dest):
                 if (f.startswith("libwarble")):
                     os.remove(os.path.join(dest, f))
+
 
 class WarbleBuild(build_py):
     @staticmethod
@@ -37,28 +48,43 @@ class WarbleBuild(build_py):
         version_mk = os.path.join(warble, "version.mk")
 
         if os.path.exists(os.path.join(root, '.git')):
-            if (call(["git", "submodule", "update", "--init", "--recursive"], cwd=root, stderr=STDOUT) != 0):
-                raise RuntimeError("Could not init git submodule")
+            _execute(
+                msg="Initializing submodules",
+                error_msg="Failed to initialize submodule",
+                args=["git", "submodule", "update", "--init", "--recursive"],
+                root=root
+            )
 
-        if (platform.system() == 'Windows'):
+        if platform.system() == 'Windows':
             args = ["MSBuild.exe", "warble.vcxproj", "/p:Platform=%s" % machine, "/p:Configuration=Release"]
             if (os.path.exists(version_mk)):
                 args.append("/p:SkipVersion=1")
 
             vs2017 = os.path.join(warble, 'vs2017')
-            if (call(args, cwd=vs2017, stderr=STDOUT) != 0):
-                raise RuntimeError("Failed to compile warble.dll")
 
+            _execute(
+                msg="Compiling Warble C++ SDK for Windows",
+                error_msg="Failed to compile warble.dll",
+                args=args,
+                root=vs2017
+            )
+
+            logging.info("Moving warble.dll to %s" % dest)
             dll = os.path.join(vs2017, "" if machine == "x86" else machine, "Release", "warble.dll")
             move(dll, dest)
-        elif (platform.system() == 'Linux'):
+        elif platform.system() == 'Linux':
             args = ["make", "-C", warble, "-j%d" % (cpu_count())]
             if (os.path.exists(version_mk)):
                 args.append("SKIP_VERSION=1")
 
-            if (call(args, cwd=root, stderr=STDOUT) != 0):
-                raise RuntimeError("Failed to compile libwarble.so")
+            _execute(
+                msg="Compiling Warble C++ SDK for Linux",
+                error_msg="Failed to compile libwarble.so",
+                args=args,
+                root=root
+            )
 
+            logging.info("Moving libwarble.so to %s" % dest)
             so = os.path.join(warble, 'dist', 'release', 'lib', machine)
             WarbleBuild._move(so, dest, 'libwarble.so')
         else:
@@ -70,7 +96,7 @@ so_pkg_data = ['libwarble.so*'] if platform.system() == 'Linux' else ['warble.dl
 setup(
     name='warble',
     packages=['mbientlab', 'mbientlab.warble'],
-    version='1.2.0',
+    version='1.2.1',
     description='Python bindings for MbientLab\'s Warble library',
     long_description=open(os.path.join(os.path.dirname(__file__), "README.rst")).read(),
     package_data={'mbientlab.warble': so_pkg_data},
